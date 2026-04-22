b505f6bca5682cee38ca86aa25731fef6f835c74
chaosbenni0609-beep <chaosbenni0609@gmail.com>
2026-04-22 05:52:04 +0000

    fragen integrieren richtiger acont

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,48 @@ ver2 = ["B"]
 ver3 = ["C"]
 list = [ver1,ver2,ver3]
 
-for i in range(3):
+for i in range(31):
         ver1.append(randint(1,5))
         ver2.append(randint(1,5))
         ver3.append(randint(1,5))
+
 
 list_without_first = [i[1:] for i in list]
 st.write(list)
 st.write(list_without_first)
 
-User_in.append(st.slider("Frage 1 :",1,5,3))
-User_in.append(st.slider("Frage 2 :",1,5,3))
-User_in.append(st.slider("Frage 3 :",1,5,3))
+User_in.append(st.slider("Wie gerne arbeite ich in einem Labor?",1,5,3))
+User_in.append(st.slider("Wie gerne arbeite ich in einem Museum?",1,5,3))
+User_in.append(st.slider("Ich arbeite gerne mit Kindern",1,5,3))
+User_in.append(st.slider("Ich arbeite gerne mit Menschen",1,5,3))
+User_in.append(st.slider("Ich arbeite gerne mit Tieren",1,5,3))
+User_in.append(st.slider("Ich mag es kreativ zu sein",1,5,3))
+User_in.append(st.slider("Ich spreche gerne andere Sprachen",1,5,3))
+User_in.append(st.slider("Ich mag es lange in der Natur zu sein",1,5,3))
+User_in.append(st.slider("Ich arbeite gerne mit Computern",1,5,3))
+User_in.append(st.slider("Ich mag es mit Zahlen zu arbeiten",1,5,3))
+User_in.append(st.slider("Ich arbeite gerne allein",1,5,3))
+User_in.append(st.slider("Ich mag Routine",1,5,3))
+User_in.append(st.slider("Ich löse gerne Probleme",1,5,3))
+User_in.append(st.slider("Ich verfasse gerne Texte",1,5,3))
+User_in.append(st.slider("Ich mag es mit meinen Händen zu arbeiten",1,5,3))
+User_in.append(st.slider("Ich mag es mich körperlich zu betätigen",1,5,3))
+User_in.append(st.slider("Ich mag es vor Leuten zu sprechen",1,5,3))
+User_in.append(st.slider("Ich mag es Verantwortung zu übernehmen",1,5,3))
+User_in.append(st.slider("Mir ist es wichtig Menschen oder Tieren zu helfen (biologisch)",1,5,3))
+User_in.append(st.slider("Ich mag es neue Themen zu erfahren",1,5,3))
+User_in.append(st.slider("Ich arbeite gerne Experimente durchzuführen",1,5,3))
+User_in.append(st.slider("Ich setze mich gerne mit theoretischen Fragen auseinander",1,5,3))
+User_in.append(st.slider("Ich setze gerne meine eigenen Ideen um",1,5,3))
+User_in.append(st.slider("Ich beschäftige mich gerne mit Kunst, Musik oder Literatur",1,5,3))
+User_in.append(st.slider("Ich lese gerne wissenschaftliche Texte",1,5,3))
+User_in.append(st.slider("Ich diskutiere gerne",1,5,3))
+User_in.append(st.slider("Ich setze mich gerne mit anderen Kulturen auseinander",1,5,3))
+User_in.append(st.slider("Ich beschäftige mich gerne mit Geschichte",1,5,3))
+User_in.append(st.slider("Ich koordiniere oder sortiere gerne",1,5,3))
+User_in.append(st.slider("Ich arbeite gerne von verschiedenen Orten",1,5,3))
+User_in.append(st.slider("Ich gehe/würde gerne oft auf Arbeitsreisen gehen",1,5,3))
+
 
 def vergleich(a,b):
     a = array(a,dtype=float)
