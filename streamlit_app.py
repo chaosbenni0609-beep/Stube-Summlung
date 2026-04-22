61c48846e8979997528e48981bd98671058823d6
chaosbenni0609-beep <chaosbenni0609@gmail.com>
2026-04-22 06:53:44 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ User_in.append(st.slider("Ich koordiniere oder sortiere gerne",1,5,3))
 User_in.append(st.slider("Ich arbeite gerne von verschiedenen Orten",1,5,3))
 User_in.append(st.slider("Ich gehe/würde gerne oft auf Arbeitsreisen gehen",1,5,3))
 
-
+bitte = [User_in]
 st.write(User_in)
 
 
